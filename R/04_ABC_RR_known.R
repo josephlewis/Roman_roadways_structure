eaf6c5a9e5845b076410076d5cbdb7fde0be9d5e
Joseph Lewis <jl2094@cam.ac.uk>
2025-11-03 14:38:28 +0000

    review amendments

--- R/04_ABC_RR_known.R
+++ R/04_ABC_RR_known.R
@@ -5,6 +5,16 @@ r <- terra::rast("./Data/DEM/OS_50m_Wales.tif")
 roads <- sf::st_read("./Data/RR_known.gpkg")
 sf::st_geometry(roads) <- "geometry"
 
+# crop DEM to Roman road ID 17 for Figure
+terra::writeRaster(terra::crop(r, sf::st_buffer(roads[roads$RR_ID == "RR642",][2,], 5000)), "./Data/DEM/OS_50m_Wales_RR17.tif", overwrite = TRUE)
+sf::st_write(roads[roads$RR_ID == "RR642",][2,], "./Data/RR17_known.gpkg", append = TRUE)
+
+terra::writeRaster(terra::crop(r, sf::st_buffer(roads[roads$RR_ID == "rr62c_3",], 5000)), "./Data/DEM/OS_50m_Wales_RR43.tif", overwrite = TRUE)
+sf::st_write(roads[roads$RR_ID == "rr62c_3",], "./Data/RR43_known.gpkg", append = TRUE)
+
+terra::writeRaster(terra::crop(r, sf::st_buffer(roads[roads$RR_ID == "RR64_2",], 5000)), "./Data/DEM/OS_50m_Wales_RR20.tif", overwrite = TRUE)
+sf::st_write(roads[roads$RR_ID == "RR64_2",], "./Data/RR20_known.gpkg", append = TRUE)
+
 b_mean <- truncnorm::rtruncnorm(n = nsims, mean = 1, sd = 10, a = 0)
 b_sd <- rexp(n = nsims, rate = 1)
 b <- truncnorm::rtruncnorm(n = nsims, mean = b_mean, sd = b_sd, a = 0)
